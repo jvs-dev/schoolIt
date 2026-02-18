f74ee48357cd81645a80105f9d228bd5ce8a4d75
jvs-dev <shepherdcom12@gmail.com>
2026-02-18 01:09:12 -0300

    update env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 const firebaseConfig = {
-  apiKey: "AIzaSyAl-5wT3Q56mG1aMaT9G8wG0rgXFUm4FZE",
-  authDomain: "schoolit-ef810.firebaseapp.com",
-  projectId: "schoolit-ef810",
-  storageBucket: "schoolit-ef810.firebasestorage.app",
-  messagingSenderId: "122657769691",
-  appId: "1:122657769691:web:303a88816ec20a92e224fe"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
